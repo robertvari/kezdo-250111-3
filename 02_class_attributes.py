07cbc20698e5f331823d0be2021cb9fded735998
Robert Vari <mail.robertvari@gmail.com>
2025-02-08 14:20:31 +0100

    update

---
+++ 02_class_attributes.py
@@ -0,0 +1,11 @@
+class Character:
+    # class attributes
+    name = "Eragon"
+    age = 24
+    inventory = ["Cheese", "Bread", "Sword"]
+
+print(Character.name, Character.age, Character.inventory)
+
+Character.name = "Csaba"
+
+print(Character.name, Character.age, Character.inventory)
