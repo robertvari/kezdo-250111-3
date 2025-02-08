2b361b71917a1b44048b46bf9f0c046d40ee1343
Robert Vari <mail.robertvari@gmail.com>
2025-02-08 17:17:00 +0100

    update

--- magicnumber_v3.py
+++ magicnumber_v3.py
@@ -26,15 +26,67 @@ class MagicNumber:
         max_tries = 3
 
         print(f"You have {max_tries} tries.")
-        print(f"MAGIC NUMBER: {self.computer.magic_number}")
+
+        self.computer.get_number()
+        self.player.get_number()
+
+        print(f"Computer number: {self.computer.magic_number}")
+
+        while self.player.magic_number != self.computer.magic_number:
+            self.clear_screen()
+            max_tries -=1
+            if max_tries == 0:
+                self.round_end_conditions()
+            
+            print(f"Wrong guess. You have {max_tries} tries left.")
+            self.player.get_number()
+    
+    def round_end_conditions(self):
+        self.clear_screen()
+
+        if self.computer.magic_number == self.player.magic_number:
+            print(f"You win {self.player}! {self.computer.magic_number} was my number")
+            self.player.add_coins(10)
+        else:
+            self.player.remove_coins(10)
+            print("You lost this round :(")
+        
+        response = self.player.ask_next_round()
+        if response == "y":
+            self.clear_screen()
+            self.game_loop()
+        else:
+            self.exit_game()
+
+    def exit_game(self):
+        self.clear_screen()
+        print("See you next time!")
+        exit()
 
 class Player:
     def __init__(self):
         self.name = None
         self.coins = 100
+        self.magic_number = 0
     
+    def ask_next_round(self):
+        return input("Do you want to play again? (y/n)")
+
+    def add_coins(self, coins):
+        self.coins += coins
+        print(f"{self.name} winds {coins} coins.")
+        print(f"Now you have {self.coins}")
+
+    def remove_coins(self, coins):
+        self.coins -= coins
+        print(f"{self.name} loses {coins} coins.")
+        print(f"Now you have {self.coins}")
+
     def get_name(self):
         self.name = input("What is your name? ")
+
+    def get_number(self):
+        self.magic_number = int(input("What is your number? "))
 
     def __str__(self):
         return self.name
@@ -43,7 +95,9 @@ class Computer:
     def __init__(self):
         self.min_number = 1
         self.max_number = 10
-        self.magic_number = random.randint(self.min_number, self.max_number)
+        self.magic_number = 0
 
+    def get_number(self):
+        self.magic_number = random.randint(self.min_number, self.max_number)
 
 MagicNumber()
