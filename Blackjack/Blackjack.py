0a5e4326dc7e6be297077cd667089b82e001a3ac
Robert Vari <mail.robertvari@gmail.com>
2025-02-09 15:18:59 +0100

    update

---
+++ Blackjack/Blackjack.py
@@ -0,0 +1,23 @@
+import os, time
+
+class Blackjack:
+    def __init__(self):
+        self.clear_screen()
+        self.intro()
+
+    def clear_screen(self):
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+    
+    def intro(self):
+        print("-"*50, "BLACKJACK", "-"*50)
+        time.sleep(1)
+        print("Blackjack is a simple card game. Your only goal is to collect cards to have 21 in your hand.")
+        time.sleep(1)
+        input("Press Enter to continue...")
+
+
+# Run game
+Blackjack()
