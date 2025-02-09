71590c2cc30f5215447e42e7ff2f252448f2201c
Robert Vari <mail.robertvari@gmail.com>
2025-02-09 14:47:51 +0100

    update

--- 13_inheritance.py
+++ 13_inheritance.py
@@ -5,7 +5,6 @@ class Player_BASE:
         self.name = None
         self.coins = 100
         self.magic_number = 0
-        self.inventory = []
     
     def get_number(self):
         self.magic_number = random.randint(1, 10)
@@ -14,9 +13,19 @@ class Player_BASE:
         first_names = ["Liam", "Emma", "Noah", "Olivia", "Ethan", "Ava", "James", "Sophia", "Benjamin", "Mia"]
         last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez"]
 
+        self.name = f"{random.choice(first_names)} {random.choice(last_names)}"
+
 
 class Player(Player_BASE):
-    pass
+    # partial method override
+    def __init__(self):
+        # runst BASE.__init__()
+        super().__init__()
+        self.coins = 200000000
+    
+    # full override
+    def get_name(self):
+        self.name = input("What is your name? ")
 
 class Computer(Player_BASE):
     pass
@@ -25,8 +34,6 @@ class Computer(Player_BASE):
 
 player = Player()
 computer = Computer()
-player.get_number()
-computer.get_number()
 
-print(player.name)
-print(computer.name)
+print(player.coins)
+print(computer.coins)
