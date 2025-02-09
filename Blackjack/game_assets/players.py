dbd43fe94200fbc7b477dac6da3ffd962db5f711
Robert Vari <mail.robertvari@gmail.com>
2025-02-09 16:59:46 +0100

    update

--- Blackjack/game_assets/players.py
+++ Blackjack/game_assets/players.py
@@ -1,6 +1,45 @@
+import random
+
 class Player_BASE:
     def __init__(self):
         self.__name = None
         self.__credits = 0
         self.__hand = []
         self.__playing = True
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+    def create(self):
+        self.__credits = random.randint(10, 100)
+        self.__name = self.__get_random_name()
+
+    def __get_random_name(self):
+        first_names = ["Liam", "Emma", "Noah", "Olivia", "Ethan", "Ava", "James", "Sophia", "Benjamin", "Mia"]
+        last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez"]
+        return f"{random.choice(first_names)} {random.choice(last_names)}"
+
+    def __str__(self):
+        return f"Name: {self.__name} Credits: {self.__credits}"
+
+class Player(Player_BASE):
+    def create(self):
+        super().create()
+        self.name = input("What is your name? ")
+
+class Computer(Player_BASE):
+    pass
+
+
+player1 = Player()
+player2 = Computer()
+player1.create()
+player2.create()
+
+print(player1.name)
+print(player2.name)
